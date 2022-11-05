138b80b838527dc2c6557c9c8d4e773f087ab2e0
evilditch <ronja@pahaoja.fi>
2022-11-05 15:13:09 +0200

    Eka käyttöliittymämuokkaus

---
+++ front-end/expo-pokeCoach/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+    <Button title='New user?' />
+    <Button title='Sign in' />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
